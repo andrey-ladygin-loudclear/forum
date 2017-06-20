3207d4990e76d185d7b6c3e2399fdb822d8c61a9
andrey <andrey@gmail.com>
2017-06-20 15:28:23 +0300

    sh

--- run.sh
+++ run.sh
@@ -7,3 +7,4 @@ docker run -e MYSQL_ALLOW_EMPTY_PASSWORD=1 -e MYSQL_DATABASE=forum -d mysql
 php artisan migrate --seed
 #npm run watch
 docker run -v "$PWD":/usr/src/app -w /usr/src/app node npm run dev
+docker exec -i 1b mysql -uroot < docker/dump.sql
